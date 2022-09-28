c355ba0f6a42b50bda743a1295268d8263acaa1e
Nikita Bakhmatov <nikitabakhmatov@gmail.com>
2022-09-28 11:40:43 +0300

    Task search and storage in LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-import React, { useState } from 'react'
-import ToDo from './ToDo'
-import ToDoForm from './ToDoForm'
+import React, { useEffect, useState } from 'react';
+import ToDo from './ToDo';
+import ToDoForm from './ToDoForm';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todoArray')));
+  const [value, setValue] = useState('');
 
+  useEffect(() => {
+    localStorage.setItem('todoArray', JSON.stringify(todos));
+  }, [todos.length]);
+
+  useEffect(() => {
+    const task = JSON.parse(localStorage.getItem('todoArray'));
+    if (task) {
+      setTodos(task);
+    }
+  }, []);
   const addTask = (userInput) => {
     if (userInput) {
       const newItem = {
         id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 1,
         task: userInput,
-        complete: false
-      }
-      setTodos([...todos, newItem])
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
     }
-  }
+  };
 
   const removeTask = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
 
   const handleToggle = (id) => {
-    const newArr = todos.slice().map((todo) =>
-      todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
-    )
-    setTodos([...newArr])
-  }
-  
+    const newArr = todos
+      .slice()
+      .map((todo) => (todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }));
+    setTodos([...newArr]);
+  };
+  const filterTask = todos.filter((item) => {
+    return item.task.toLowerCase().includes(value.toLowerCase());
+  });
   return (
     <div className="App">
+      <form>
+        <input
+          type="text"
+          // className=formSearch
+          name="search"
+          placeholder="Search"
+          onChange={(event) => setValue(event.target.value)}
+        />
+      </form>
       <header>
         <h1>Список задач: {todos.length}</h1>
       </header>
       <ToDoForm addTask={addTask} />
-      {todos.map((todo) => {
-        return (
-          <ToDo
-            todo={todo}
-            key={todo.id}
-            toggleTask={handleToggle}
-            removeTask={removeTask}
-          />
-        )
+      {filterTask.map((todo) => {
+        return <ToDo todo={todo} key={todo.id} toggleTask={handleToggle} removeTask={removeTask} />;
       })}
     </div>
   );
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
 
 function ToDo({ todo, toggleTask, removeTask }) {
-    return (
-        <div key={todo.id} className="item-todo">
-            <div onClick={() => toggleTask(todo.id)} className={todo.complete ? "item-text strike" : "item-text"}>
-                {todo.task}
-            </div>
-            <div className="item-delete" onClick={() => removeTask(todo.id)}>
-                X
-            </div>
-        </div>
-    )
+  return (
+    <div key={todo.id} className="item-todo">
+      <div
+        onClick={() => toggleTask(todo.id)}
+        className={todo.complete ? 'item-text strike' : 'item-text'}>
+        {todo.task}
+      </div>
+      <div className="item-delete" onClick={() => removeTask(todo.id)}>
+        x
+      </div>
+    </div>
+  );
 }
 
 export default ToDo;
